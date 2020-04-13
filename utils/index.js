688754214ac98b12d32472d38a44e178cbd162d3
Samuel Jeancarlos Mancebo Paula <sjmancebo@gmail.com>
2020-04-13 13:15:54 -0400

    Changed default exports from index to allow export multiple images resolvers

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,20 @@
 const getImageFromBucket = require('./getImageFromBucket');
+const getImageFromUrl = require('./getImageFromUrl');
+
+const url = require('url');
+
+const getImage = (nameOrUrl) => {
+  const link = url.parse(nameOrUrl);
+
+  if(link.protocol) {
+    return getImageFromUrl(nameOrUrl)
+  } else {
+    return getImageFromBucket(nameOrUrl);
+  }
+}
 
 module.exports = {
-  getImageFromBucket
+  getImageFromBucket,
+  getImageFromUrl,
+  getImage,
 }
