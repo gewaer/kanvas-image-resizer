5039fc9d9f50f7cb2b50503b510f66af656e8f70
Samuel Jeancarlos Mancebo Paula <sjmancebo@gmail.com>
2020-04-13 13:14:49 -0400

    Added a default get route to process images from url

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sharp = require('sharp');
-const { getImageFromBucket } = require('../utils');
+const { getImage } = require('../utils');
 
 router.get('/:name', async (req, res) => {
   const { name } = req.params;
@@ -15,9 +15,10 @@ router.get('/:name', async (req, res) => {
 
   const width = w ? Number(w) : undefined;
   const height = h ? Number(h) : undefined;
+  
 
   try {
-    const originaImageBuffer = await getImageFromBucket(name);
+    const originaImageBuffer = await getImage(name);
     const transformedImage = await sharp(originaImageBuffer)
     .resize({
       ...width && { width },
@@ -35,9 +36,41 @@ router.get('/:name', async (req, res) => {
   }catch(e) {
     res.json({ msg: e.message })
   }
+})
+
+router.get('/*', async (req,res) => {
+  const name = req.url.slice(1)
+  const {
+    w = null,
+    h = null,
+    fit = 'cover',
+    position = 'center',
+    gravity = undefined,
+    strategy = undefined
+  } = req.query
+
+  const width = w ? Number(w) : undefined;
+  const height = h ? Number(h) : undefined;
   
-  
-  
+
+  try {
+    const originaImageBuffer = await getImage(name);
+    const transformedImage = await sharp(originaImageBuffer)
+    .resize({
+      ...width && { width },
+      ...height && { height },
+      ...gravity && { gravity },
+      ...strategy && { strategy },
+      position
+    })
+    .toFormat('jpeg')
+    .toBuffer();
+
+    res.contentType('image/jpeg')
+    res.end(transformedImage, 'binary');
+  }catch(e) {
+    res.json({ msg: e.message })
+  }
 })
 
 module.exports = router;
