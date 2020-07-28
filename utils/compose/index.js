85e2123c46e46b1977101eb8de103f70f53f624b
Samuel Jeancarlos Mancebo Paula <sjmancebo@gmail.com>
2020-07-28 13:50:55 -0400

    fixed a bug with multiline text and added a gap between image

--- utils/compose/index.js
+++ utils/compose/index.js
@@ -1,7 +1,50 @@
 const { createCanvas, loadImage } = require('canvas');
 const sharp = require('sharp');
 const AMAZON_S3_BUCKET = process.env.AMAZON_S3_BUCKET;
+const drawMultilineText = require('canvas-multiline-text');
 
+const GAP = 10;
+
+const wrapText = (context, text, x, y, maxWidth, lineHeight) => {
+  var words = text.split(' ');
+  var line = '';
+
+  for(var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + ' ';
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      context.fillText(line, x, y);
+      line = words[n] + ' ';
+      y += lineHeight;
+    }
+    else {
+      line = testLine;
+    }
+  }
+  context.fillText(line, x, y);
+}
+
+const calculateWrappedTextHeight = (context, text, y, maxWidth, lineHeight) => {
+  var words = text.split(' ');
+  var line = '';
+
+  for(var n = 0; n < words.length; n++) {
+    var testLine = line + words[n] + ' ';
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      line = words[n] + ' ';
+      y += lineHeight;
+    }
+    else {
+      line = testLine;
+    }
+  }
+
+  return y;
+  
+}
 
 const composeImage = (imgs, alts, width = 667, height = 500, backgroundColor='#262628') => {
   return new Promise(async (resolve, reject) => {
@@ -27,16 +70,18 @@ const composeImage = (imgs, alts, width = 667, height = 500, backgroundColor='#2
             context.drawImage(image, cellX, y, perImageWidth, height)
           } else {
             const text = alts[index];
-            context.font = "bold 30pt Menlo";
-            context.textAlign = 'center';
+            context.font = "bold 25pt Menlo";
+            context.textAlign = 'left';
             context.fillStyle = '#fff';
-            const textHeight = 30
-            const textWidth = context.measureText(text).width
-            x = (cellX + perImageWidth / 2);
+            const textHeight = calculateWrappedTextHeight(context, text, y, perImageWidth, 40)
+            x = (cellX + GAP);
             y = (height / 2) - (textHeight / 2)
-            context.fillText(text, x, y);
+            if (textHeight >= 200) {
+              text += '...'
+            }
+            wrapText(context, text, x, y, perImageWidth - 25, 40)
           }
-          cellX = perImageWidth * (index + 1);
+          cellX = perImageWidth * (index + 1) + GAP;
         }
    
         const buffer = canvas.toBuffer("image/jpeg");
